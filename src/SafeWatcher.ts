dd631f96268fb3de5d616d57715aca369332d712
Bernardo Codesido <bernardo@iovlabs.org>
2025-03-06 23:46:36 -0300

    feat: add descriptor/name to safe addresses

--- src/SafeWatcher.ts
+++ src/SafeWatcher.ts
@@ -24,6 +24,7 @@ interface SafeWatcherOptions {
 class SafeWatcher {
   readonly #prefix: string;
   readonly #safe: Address;
+  readonly #name: string;
   readonly #notificationSender?: INotificationSender;
   readonly #logger: Logger;
   readonly #api: ISafeAPI;
@@ -33,10 +34,11 @@ class SafeWatcher {
   #interval?: NodeJS.Timeout;
 
   constructor(opts: SafeWatcherOptions) {
-    const [prefix, address] = parsePrefixedAddress(opts.safe);
+    const [prefix, address, name] = parsePrefixedAddress(opts.safe);
     this.#logger = logger.child({ chain: prefix, address });
     this.#prefix = prefix;
     this.#safe = address;
+    this.#name = name;
     this.#notificationSender = opts.notifier;
     this.#signers = opts.signers ?? {};
     this.#api = new SafeApiWrapper(opts.safe, opts.api);
@@ -125,6 +127,7 @@ class SafeWatcher {
 
     await this.#notificationSender?.notify({
       type: isMalicious ? "malicious" : "created",
+      name: this.#name,
       chainPrefix: this.#prefix,
       safe: this.#safe,
       tx: detailed,
@@ -153,6 +156,7 @@ class SafeWatcher {
 
     await this.#notificationSender?.notify({
       type: tx.isExecuted ? "executed" : "updated",
+      name: this.#name,
       chainPrefix: this.#prefix,
       safe: this.#safe,
       tx: detailed,
